18a12a35fa5fda6ca527f8f4912882c4a0d85073
Ji Xi Yang <jixiyang@Jis-MacBook-Pro.local>
2020-11-14 16:26:42 -0500

    boilerplate code for frontend

---
+++ frontend/config/defaultSettings.ts
@@ -0,0 +1,22 @@
+import { Settings as ProSettings } from '@ant-design/pro-layout';
+
+type DefaultSettings = Partial<ProSettings> & {
+  pwa: boolean;
+};
+
+const proSettings: DefaultSettings = {
+  navTheme: 'light',
+  primaryColor: '#1890ff',
+  layout: 'side',
+  contentWidth: 'Fluid',
+  fixedHeader: false,
+  fixSiderbar: true,
+  colorWeak: false,
+  title: 'Smart Search',
+  pwa: false,
+  iconfontUrl: '',
+};
+
+export type { DefaultSettings };
+
+export default proSettings;
---
+++ frontend/mock/user.ts
@@ -0,0 +1,7 @@
+export default {
+  'GET /api/currentUser': {
+    name: 'John Cena',
+    avatar: 'https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png',
+    userid: '00000001',
+  },
+};
